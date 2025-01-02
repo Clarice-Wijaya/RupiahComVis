98dd04f045f139595ebd90ce033060c791cbd39d
Clarice-Wijaya <clarice.wijaya@binus.edu>
2025-01-02 19:28:01 +0700

    commit

---
+++ machine_learning.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from utils import load_pickle, extract_features, labels
+from camera_input_live import camera_input_live
+import numpy as np
+import cv2
+from streamlit import session_state as state
+
+st.title("Machine Learning")
+
+mode = st.segmented_control("Select Mode", ["Realtime Camera", "Upload Image"], default="Upload Image", selection_mode="single")
+selection = st.selectbox("Choose model", [
+    "K-Nearest Neighbour",
+    "Support Vector Machine",
+    "Random Forest"
+])
+
+if mode == "Realtime Camera":
+  image = camera_input_live(500)
+elif mode == "Upload Image":
+  image = st.file_uploader("Upload an Image", type=['jpg', 'png'])
+
+if image:
+  image = np.asarray(bytearray(image.read()), dtype="uint8") 
+  image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+  # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+  image = cv2.resize(image, (512, 512))
+  features = extract_features(image)
+  # st.write(features)
+  # st.write(selection)
+  model = state['models'][selection]
+  pred = model.predict([features])
+  st.write(labels[int(pred)])
+  st.image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
---
+++ utils.py
@@ -0,0 +1,85 @@
+import pickle
+import cv2
+import numpy as np
+import gzip
+
+import torch
+from torchvision import models
+from ultralytics import YOLO
+
+labels = ['1 RIBU ASLI', '1 RIBU PALSU', '10 RIBU ASLI', '10 RIBU PALSU', 
+         '100 RIBU ASLI', '100 RIBU PALSU', '20 RIBU ASLI', '20 RIBU PALSU', 
+         '5 RIBU ASLI', '5 RIBU PALSU', '50 RIBU ASLI', '50 RIBU PALSU']
+
+def load_pickle(file_path):
+    with open(file_path, 'rb') as file:
+        return pickle.load(file)
+
+# microtext
+def extract_microtext(image_gray):
+    edges = cv2.Canny(image_gray, 50, 150)
+    return np.sum(edges) / edges.size
+
+# warna
+def extract_color(image):
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hue_mean = np.mean(hsv[:, :, 0])
+    sat_mean = np.mean(hsv[:, :, 1])
+    val_mean = np.mean(hsv[:, :, 2])
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    intensity_mean = np.mean(gray)
+
+    red_mean = np.mean(image[:, :, 2])
+    green_mean = np.mean(image[:, :, 1])
+    blue_mean = np.mean(image[:, :, 0])
+
+    return hue_mean, sat_mean, val_mean, intensity_mean, red_mean, green_mean, blue_mean
+
+# benang pengaman
+def detect_thread(image_gray):
+    _, thresh = cv2.threshold(image_gray, 128, 255, cv2.THRESH_BINARY)
+    vertical_kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (1, 15))
+    thread = cv2.morphologyEx(thresh, cv2.MORPH_OPEN, vertical_kernel)
+    return np.sum(thread) / thread.size
+
+
+def extract_features(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    gray=cv2.equalizeHist(gray)
+    microtext_density = extract_microtext(gray)
+    hue, saturation, value, intensity, red, green, blue = extract_color(image)
+    thread_density = detect_thread(gray)
+    return [microtext_density, hue, saturation, value, intensity, red, green, blue, thread_density]
+    
+
+class ResNet50(torch.nn.Module):
+    def __init__(self):
+        super(ResNet50, self).__init__()
+        self.resnet = models.resnet50(pretrained=True)
+        self.resnet.fc = torch.nn.Linear(in_features=2048, out_features=12)
+        
+    def forward(self, x):
+        return self.resnet(x)
+    
+class VGG19(torch.nn.Module):
+    def __init__(self):
+        super(VGG19, self).__init__()
+        self.vgg19 = models.vgg19(pretrained=True)
+        self.vgg19.fc = torch.nn.Linear(in_features=2048, out_features=12)
+        # self.resnet.compile()
+        
+    def forward(self, x):
+        return self.vgg19(x)
+
+def load_pt(model_name=None):
+    if model_name == "ResNet50":
+        model = ResNet50()
+        # model = VGG19()
+        model.load_state_dict(torch.load('resnet50-4.pth'))
+        model = model.to('cuda')
+        model.eval()
+    if model_name == "YoLoV11":
+        model = YOLO("last-4.pt")
+    return model
+
